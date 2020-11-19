7d8cee6e30515a9b1eba0e98f114c0b7a64ba464
s452737 <lukbie2@st.amu.edu.pl>
2020-11-19 23:36:40 +0100

    Commit 2

--- PracowaniaProgramowania/src/main/java/second/shortcuts/ClassThatHaveItAll.java
+++ PracowaniaProgramowania/src/main/java/second/shortcuts/ClassThatHaveItAll.java
@@ -1,6 +1,22 @@
 package second.shortcuts;
 
+import java.util.List;
+
 public class ClassThatHaveItAll implements InterfaceOne {
 
-    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ClassThatHaveItAll.class); String name; Integer number; public void printMe(String info) { logger.info(info); } public void usageOfPrint() { printMe("Hi"); }
+    List<Long> list;
+
+    final static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(ClassThatHaveItAll.class);
+    String name;
+    Integer number;
+
+
+
+    public void printMe(String info) {
+        logger.info(info);
+    }
+
+    public void usageOfPrint() {
+        printMe("Hi");
+    }
 }
